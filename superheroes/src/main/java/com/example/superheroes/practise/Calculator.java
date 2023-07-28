e3927c42162f4f3ddf1d362fccefad0ef6b5ce63
malturki42 <mtalturki42@tntech.edu>
2023-07-28 20:59:36 +0100

    Spring Boot QL1-7

---
+++ superheroes/src/main/java/com/example/superheroes/practise/Calculator.java
@@ -0,0 +1,9 @@
+package com.example.superheroes.practise;
+
+import org.springframework.stereotype.Component;
+@Component
+public class Calculator {
+    public int addition(int num1, int num2){
+        return num1+num2;
+    }
+}
