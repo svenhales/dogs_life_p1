b0cd77660690481c35bb6cbdd5f5a0957fd51d84
svenhales <svenhales@gmail.com>
2023-07-28 21:39:01 +0100

    ql6

---
+++ superheroes/src/main/java/com/example/superheroes/Service/HeroService.java
@@ -0,0 +1,26 @@
+package com.example.superheroes.Service;
+
+import com.example.superheroes.model.Hero;
+import com.example.superheroes.repository.HeroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class HeroService {
+    @Autowired
+    private HeroRepository heroRepository;
+    public List<Hero> getHeroesStartingWithLetter(String letter){
+        String letterParam = letter + "%";
+        List<Hero> heroes = heroRepository.findHeroesNameStartingWithLetter(letterParam);
+        return heroes;
+    }
+    public List<Hero> getAllHeroes(){
+        return heroRepository.findAll();
+    }
+
+    public List<Hero> getLastThreeHeroesByName(){
+        return heroRepository.findTop3ByOrderByNameDesc();
+    }
+}
