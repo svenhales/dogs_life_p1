fb3c8e3581bcc480301f5ff6c364a5a6605fe787
Rahul Kapoor <rahulkapp@hotmail.com>
2023-08-01 19:27:34 +0100

    QL8

--- ReactPractice/superheroes-ui/src/services/hero-service.js
+++ ReactPractice/superheroes-ui/src/services/hero-service.js
@@ -4,18 +4,6 @@ export function getAllHeroes(){
     return http.get("/heroes");
 }
 
-const [heroes,setHeroes] = useState([]);
-useEffect(()=>{
-    getHeroesFromAPI();}, 
-    []
-);
-const getHeroesFromAPI = ()=>{}
-
-getAllHeroes()
-  .then(res => {
-      setHeroes(res.data);
-  })
-  .catch(err => {
-      setHeroes([]);
-      console.log(err);
-  })
+export function saveHero(hero){
+    return http.post("/heroes", hero)
+}
