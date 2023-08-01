510d632813b0001d613f551282bdb0862243228e
yaldamauj <yalda.isk@gmail.com>
2023-08-01 18:33:42 +0100

    working on QL3

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import HeroDetail from './HeroDetail'
+import React from 'react';
+import images from '../index';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const AllHeroes = () => {
+const HeroDetail = (props) => {
   return (
-   <HeroDetail/>
+    <Card >
+      <div className="imgContainer">
+        <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
+      </div>
+      <Card.Body >
+        <Card.Title >Alias: {props.info.alias}</Card.Title>
+        <Card.Text >Name: {props.info.name}</Card.Text>
+        <Button variant="primary">Like</Button>
+      </Card.Body>
+    </Card>
   )
 }
 
-export default AllHeroes
+export default HeroDetail
