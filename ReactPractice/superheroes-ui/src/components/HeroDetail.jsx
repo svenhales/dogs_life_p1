34174c6eb3210251aec8049a94f8bceff443a0b5
malturki42 <mtalturki42@tntech.edu>
2023-08-01 13:58:10 +0100

    QL3 DONE

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-//import batman from '../images/batman.jpeg';
+
+import React from 'react';
 import images from '../index';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const HeroDetail = (props) => {
-
-    return (
-      <div>
-        <img src={images[props.info.alias.replaceAll(/\s/g,'')]} alt={props.info.name} height="250px"/>
-        <p id="blue-alias">Alias: {props.info.alias}</p>
-        <p className="green-class">Name: {props.info.name}</p>
-        <button className="green-class">Like</button>   
+  return (
+    <Card >
+      <div className="imgContainer">
+        <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
       </div>
-    )
-  }
+      <Card.Body >
+        <Card.Title >Alias: {props.info.alias}</Card.Title>
+        <Card.Text >Name: {props.info.name}</Card.Text>
+        <Button variant="primary">Like</Button>
+      </Card.Body>
+    </Card>
+  )
+}
 
 export default HeroDetail
 
