2c22412927dff05e34cafcf24abd9932434310ab
yaldamauj <yalda.isk@gmail.com>
2023-08-01 21:17:46 +0100

    finished QL7

--- ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
+++ ReactPractice/superheroes-ui/src/components/HeroDetail.jsx
@@ -2,17 +2,33 @@ import React from 'react';
 import images from '../index';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { useState } from 'react'; 
+import Row from 'react-bootstrap/esm/Row';
+import Col from 'react-bootstrap/Col';
+
 
 const HeroDetail = (props) => {
+  
+  const[counter, setCounter] = useState(0);
+  const incrementCounter =()=>{
+    setCounter(counter+1);
+    props.parentCount(props.info.alias);
+}
+
+  
   return (
     <Card >
+      
       <div className="imgContainer">
         <Card.Img variant="top" src={images[props.info.alias.replaceAll(/\s/g, '')]} width="150px" />
       </div>
       <Card.Body >
         <Card.Title >Alias: {props.info.alias}</Card.Title>
         <Card.Text >Name: {props.info.name}</Card.Text>
-        <Button variant="primary">Like</Button>
+        <Row>
+          <Col><Button variant="primary" onClick={incrementCounter}>Like</Button></Col>
+          <Col>{counter > 0  && <Card.Text >Likes: {counter}</Card.Text > }</Col>
+        </Row>
       </Card.Body>
     </Card>
   )
